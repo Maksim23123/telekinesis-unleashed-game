921eb0b11c1d98477df705bc04fa4fea7e4b38f8
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-06-03 14:22:43 +0200

    Fix Target visibility miscalculation bug

--- Assets/Scripts/Enemies/EnemyProjectileLauncher.cs
+++ Assets/Scripts/Enemies/EnemyProjectileLauncher.cs
@@ -74,10 +74,54 @@ public class EnemyProjectileLauncher : MonoBehaviour
 
     private void TestIfTargetIsVisible(Vector2 direction)
     {
-        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, direction, 20, _notTransparentLayers);
-        if (raycastHit.transform != null && raycastHit.transform.gameObject == _target)
-            TargetIsVisible = true;
-        else
-            TargetIsVisible = false;
+        TargetIsVisible = false;
+        Vector2 objectPosition = gameObject.transform.position;
+        float raySize = 20;
+        if (Vector2.Distance(objectPosition, _target.transform.position) <= raySize)
+        {
+            float rayThickness = 0.4f;
+            RaycastHit2D[] hits = Physics2D.BoxCastAll(objectPosition, new Vector2(rayThickness, rayThickness)
+                , Vector2.Angle(Vector2.right, direction), direction, raySize, _notTransparentLayers);
+
+            DebugDrawBoxCast(objectPosition, new Vector2(rayThickness, rayThickness)
+                , Vector2.Angle(Vector2.right, direction), direction, raySize, Color.red);
+
+            if (hits.Length >= 1 && hits[0].transform.gameObject == _target)
+                TargetIsVisible = true;
+        }
+    }
+
+    void DebugDrawBoxCast(Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, Color color)
+    {
+        Vector2[] corners = new Vector2[4];
+        Quaternion rotation = Quaternion.Euler(0, 0, angle);
+
+        corners[0] = origin + (Vector2)(rotation * new Vector2(-size.x / 2, -size.y / 2));
+        corners[1] = origin + (Vector2)(rotation * new Vector2(size.x / 2, -size.y / 2));
+        corners[2] = origin + (Vector2)(rotation * new Vector2(size.x / 2, size.y / 2));
+        corners[3] = origin + (Vector2)(rotation * new Vector2(-size.x / 2, size.y / 2));
+
+        Debug.DrawLine(corners[0], corners[1], color);
+        Debug.DrawLine(corners[1], corners[2], color);
+        Debug.DrawLine(corners[2], corners[3], color);
+        Debug.DrawLine(corners[3], corners[0], color);
+
+        // Draw the movement direction of the BoxCast
+        Vector2 endPoint = origin + direction.normalized * distance;
+        Debug.DrawLine(origin, endPoint, color);
+
+        // Draw the box at the end position of the BoxCast
+        Vector2 endOrigin = endPoint;
+        Vector2[] endCorners = new Vector2[4];
+
+        endCorners[0] = endOrigin + (Vector2)(rotation * new Vector2(-size.x / 2, -size.y / 2));
+        endCorners[1] = endOrigin + (Vector2)(rotation * new Vector2(size.x / 2, -size.y / 2));
+        endCorners[2] = endOrigin + (Vector2)(rotation * new Vector2(size.x / 2, size.y / 2));
+        endCorners[3] = endOrigin + (Vector2)(rotation * new Vector2(-size.x / 2, size.y / 2));
+
+        Debug.DrawLine(endCorners[0], endCorners[1], color);
+        Debug.DrawLine(endCorners[1], endCorners[2], color);
+        Debug.DrawLine(endCorners[2], endCorners[3], color);
+        Debug.DrawLine(endCorners[3], endCorners[0], color);
     }
 }
