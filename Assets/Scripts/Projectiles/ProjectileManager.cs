9c278c413c88e5376e4a255bd13c9118927015a0
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-06-07 06:44:53 +0200

    Adapt bullet rotation to launch direction

--- Assets/Scripts/Projectiles/ProjectileManager.cs
+++ Assets/Scripts/Projectiles/ProjectileManager.cs
@@ -11,6 +11,11 @@ public class ProjectileManager : MonoBehaviour
     {
         if (TryGetComponent(out Rigidbody2D rigidbody) && _projectileProperties != null)
         {
+            if (Vector2.Angle(Vector2.down, direction) > 90)
+                transform.rotation = Quaternion.Euler(Vector2.Angle(Vector2.right, direction) * new Vector3(0, 0, 1));
+            else
+                transform.rotation = Quaternion.Euler( Vector2.Angle(Vector2.left, direction) * new Vector3(0, 0, 1));
+
             rigidbody.AddForce(direction * _projectileProperties._impulsPower, ForceMode2D.Impulse);
         }
     }
