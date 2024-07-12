af2a8d222c00104838cb437d3ecd94d93bd3866b
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-07-12 15:02:58 +0200

    Refactor PossesableObjectDamageHandler

--- Assets/Scripts/Damage Handlers/PossesableObjectDamageHandler.cs
+++ Assets/Scripts/Damage Handlers/PossesableObjectDamageHandler.cs
@@ -4,14 +4,14 @@ public class PossesableObjectDamageHandler : DamageHandler
 {
     private EntityHealthManager _healthManager;
 
+    private void Start()
+    {
+        _healthManager = GetComponent<EntityHealthManager>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent(out PossessableObject capturableObject))
             _healthManager.ProcessDamage((int)Mathf.Clamp(capturableObject.GetContactDamage() - GetCurrentResistanceAmount(), 0, int.MaxValue));
-    }
-
-    private void Start()
-    {
-        _healthManager = GetComponent<EntityHealthManager>();
     }
 }
