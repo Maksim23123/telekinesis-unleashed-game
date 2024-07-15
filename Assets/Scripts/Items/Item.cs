c2d56602a6adfd3a148741e6dd4d5f942b2bd949
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-07-15 10:30:02 +0200

    Refactor Item

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,12 +2,9 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    [SerializeField]
-    private int _itemID;
-    [SerializeField]
-    private PlayerStatsStorage _statsModifier;
-    [SerializeField]
-    private ItemEvent _itemEvent;
+    [SerializeField] private int _itemID;
+    [SerializeField] private PlayerStatsStorage _statsModifier;
+    [SerializeField] private ItemEvent _itemEvent;
 
     public PlayerStatsStorage StatsModifier { get => _statsModifier; set => _statsModifier = value; }
     public ItemEvent ItemEvent { get => _itemEvent; }
