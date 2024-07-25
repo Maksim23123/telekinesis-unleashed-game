2901a19ed44f96cb431e89099d46c034437206cb
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-07-25 14:49:22 +0200

    Add comments for DamageHandler class

--- Assets/Scripts/Damage Handlers/DamageHandler.cs
+++ Assets/Scripts/Damage Handlers/DamageHandler.cs
@@ -1,5 +1,14 @@
 using UnityEngine;
 
+/// <summary>
+/// Abstract class for handling damage. This class requires an 
+/// <see cref="EntityHealthManager"/> component to be attached to the same GameObject.
+/// </summary>
+/// <remarks>
+/// The <c>DamageHandler</c> class provides a base implementation for damage handling,
+/// including a resistance property that can be modified. It also includes a method to 
+/// calculate a random resistance amount based on the current resistance value.
+/// </remarks>
 [RequireComponent(typeof(EntityHealthManager))]
 public abstract class DamageHandler : MonoBehaviour
 {
@@ -7,6 +16,10 @@ public abstract class DamageHandler : MonoBehaviour
 
     public float Resistance { get => _resistance; set => _resistance = value; }
 
+    /// <summary>
+    /// Calculates a random resistance amount based on the current resistance value.
+    /// </summary>
+    /// <returns>A random resistance amount.</returns>
     protected float GetCurrentResistanceAmount()
     {
         return Random.value * Resistance;
