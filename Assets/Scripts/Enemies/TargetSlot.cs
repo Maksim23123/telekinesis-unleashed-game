41438e10d30c3a122fc4b69948f153578a7439d9
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-07-26 17:40:51 +0200

    Refactor TargetSlot class and add comments for clarity

--- Assets/Scripts/Enemies/TargetSlot.cs
+++ Assets/Scripts/Enemies/TargetSlot.cs
@@ -1,26 +1,21 @@
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetSlot : IComparer<TargetSlot>
+/// <summary>
+/// Represents a slot for a target with related properties used in <see cref="EnemyTargetManager"/>.
+/// This class is intended to be stored and used within the <see cref="EnemyTargetManager"/>.
+/// </summary>
+public class TargetSlot
 {
     public int TargetPriority { get; set; }
     public TargetType TargetType { get; set; }
     public GameObject Target { get; set; }
     public bool TargetIsVisible { get; set; }
 
+
     public TargetSlot(int targetPriority, TargetType targetType, GameObject target)
     {
         TargetPriority = targetPriority;
         TargetType = targetType;
         Target = target;
-    }
-
-    public int Compare(TargetSlot x, TargetSlot y)
-    {
-        int comparisonResult = x.TargetPriority.CompareTo(y.TargetPriority);
-        if (comparisonResult == 0)
-            return 1;
-        else
-            return comparisonResult;
     }
 }
