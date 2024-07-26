ffd52d1b39744608a577a09b21a2ea8e49d76926
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-07-26 13:30:12 +0200

    Refactor and complement with comments EnemyTargetManager class

--- Assets/Scripts/Enemies/EnemyTargetManager.cs
+++ Assets/Scripts/Enemies/EnemyTargetManager.cs
@@ -3,46 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/// <summary>
+/// This class tracks potential targets nearby. It also check target visibility.
+/// </summary>
+/// <remarks>
+/// Sends all the information about targets to <c>ActivateTargetReAssigned</c> event subscribers.
+/// </remarks>
 public class EnemyTargetManager : MonoBehaviour
 {
     [SerializeField] private LayerMask _notTransparentLayers;
     [SerializeField] private float switchToMinorDistance;
 
-    private EnemyProjectileLauncher _launcher;
-    private SortedSet<TargetSlot> _targetSlots = new SortedSet<TargetSlot>(new TargetSlot(0, TargetType.Main, null));
+    private List<TargetSlot> _targetSlots = new();
     private TargetSlot previousMostPriorSlot;
 
     public event Action<GameObject, bool> ActiveTargetReAssigned;
 
+    /// <summary>
+    /// Subscribes to NewPlayerAssigned event, as the player object is the main target.
+    /// </summary>
     private void Start()
     {
         PlayerStatusInformer.NewPlayerAssigned += UpdateTargets;
     }
 
+    /// <summary>
+    /// Periodically updates target references.
+    /// </summary>
     private void FixedUpdate()
     {
         if (PlayerStatusInformer.PlayerGameObject != null)
             UpdateTargets(PlayerStatusInformer.PlayerGameObject);
     }
 
-    private void Awake()
-    {
-        TryGetComponent(out _launcher);
-    }
-
+    /// <summary>
+    /// Chooses the target with the highest priority and informs subscribers.
+    /// </summary>
     private void AssignTarget()
     {
-        TargetSlot mostPriorSlot = _targetSlots.Max; 
+        TargetSlot mostPriorSlot = _targetSlots
+            .OrderBy(targetSlot => targetSlot.TargetPriority)
+            .FirstOrDefault();
 
-        if (mostPriorSlot != null && (previousMostPriorSlot == null
-                || previousMostPriorSlot.Target != mostPriorSlot.Target
-                || previousMostPriorSlot.TargetIsVisible != mostPriorSlot.TargetIsVisible))
+        bool currentTargetDifferentFromPrevious = (previousMostPriorSlot == null
+                        || previousMostPriorSlot.Target != mostPriorSlot.Target
+                        || previousMostPriorSlot.TargetIsVisible != mostPriorSlot.TargetIsVisible);
+        if (mostPriorSlot != null && currentTargetDifferentFromPrevious)
         {
             previousMostPriorSlot = mostPriorSlot;
             ActiveTargetReAssigned?.Invoke(mostPriorSlot.Target, mostPriorSlot.TargetIsVisible);
         }
     }
 
+    /// <summary>
+    /// Updates available targets in the targetSlots list.
+    /// </summary>
+    /// <param name="mainTarget">The main target, usually the Player GameObject.</param>
     private void UpdateTargets(GameObject mainTarget)
     {
         _targetSlots.Clear();
@@ -52,16 +68,24 @@ public class EnemyTargetManager : MonoBehaviour
         AssignTarget();
     }
 
+    /// <summary>
+    /// Recalculates priority for all the targets in the targetSlots list.
+    /// </summary>
     private void RecalculateTargetsPriority()
     {
-        List<TargetSlot> slotsList = _targetSlots.ToList();
+        List<TargetSlot> targetSlotsCopy = _targetSlots.ToList();
         _targetSlots.Clear();
-        foreach (TargetSlot slot in slotsList) 
+        foreach (TargetSlot slot in targetSlotsCopy) 
         {
             _targetSlots.Add(CalculatePriorityForTarget(slot));
         }
     }
 
+    /// <summary>
+    /// Calculates priority for the specified target.
+    /// </summary>
+    /// <param name="targetSlot">The specified target.</param>
+    /// <returns>Copy of the specified target with a new priority value.</returns>
     private TargetSlot CalculatePriorityForTarget(TargetSlot targetSlot)
     {
         targetSlot.TargetIsVisible = true;
@@ -82,23 +106,31 @@ public class EnemyTargetManager : MonoBehaviour
         return targetSlot;
     }
 
+    /// <summary>
+    /// Performs a circle cast to measure target visibility.
+    /// </summary>
+    /// <param name="targetSlot">The specified target.</param>
+    /// <returns>Target visibility value.</returns>
     private bool TestIfTargetIsVisible(TargetSlot targetSlot)
     {
         bool isVisible = false;
         GameObject target = targetSlot.Target;
+
         Vector2 objectPosition = gameObject.transform.position;
         Vector2 direction = (target.transform.position - transform.position).normalized;
+
         float raySize = 20;
         if (targetSlot.TargetType == TargetType.Minor)
         {
             raySize = switchToMinorDistance;
         }
+
         if (Vector2.Distance(objectPosition, target.transform.position) <= raySize)
         {
             float rayThickness = 0.4f;
             RaycastHit2D[] hits = Physics2D.CircleCastAll(objectPosition, rayThickness / 2, direction, raySize, _notTransparentLayers);
 
-            DebugDrawBoxCast(objectPosition, new Vector2(rayThickness, rayThickness)
+            DebugDrawCircleCast(objectPosition, new Vector2(rayThickness, rayThickness)
                 , Vector2.Angle(Vector2.right, direction), direction, raySize, Color.red);
 
             if (hits.Length >= 1 && hits[0].transform.gameObject == target)
@@ -110,7 +142,14 @@ public class EnemyTargetManager : MonoBehaviour
         return isVisible;
     }
 
-    private void DebugDrawBoxCast(Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, Color color)
+    /// <summary>
+    /// Debug feature to show circle cast in Scene view.
+    /// </summary>
+    /// <param name="color">Color of the displayed cast.</param>
+    /// <remarks>
+    /// All parameters (except "color") should be transferred from the circle cast it displays.
+    /// </remarks>
+    private void DebugDrawCircleCast(Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, Color color)
     {
         Vector2[] corners = new Vector2[4];
         Quaternion rotation = Quaternion.Euler(0, 0, angle);
@@ -142,6 +181,9 @@ public class EnemyTargetManager : MonoBehaviour
         Debug.DrawLine(endCorners[3], endCorners[0], color);
     }
 
+    /// <summary>
+    /// Automatically unsubscribes from all subscriptions on destroy.
+    /// </summary>
     private void OnDestroy()
     {
         PlayerStatusInformer.NewPlayerAssigned -= UpdateTargets;
