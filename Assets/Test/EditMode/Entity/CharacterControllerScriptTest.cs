409fa63f3c98a25189d2887a144ec3172de40b18
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-07-31 14:58:23 +0200

    Add unit test for CharacterControllerScript.DirectionalFactor

---
+++ Assets/Test/EditMode/Entity/CharacterControllerScriptTest.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class CharacterControllerScriptTest
+{
+    GameObject _gameObject;
+
+    CharacterControllerScript _characterControllerScript;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _gameObject = new GameObject();
+        _characterControllerScript = _gameObject.AddComponent<CharacterControllerScript>();
+    }
+
+    //MethodName_StateUnderTest_ExpectedBehavior
+    [Test]
+    [TestCase(1)]
+    [TestCase(-1)]
+    [TestCase(0)]
+    [TestCase(5)]
+    [TestCase(-10)]
+    public void DirectionalFactor_IsWithinRange_True(int initialDirectionalFactorValue)
+    {
+        _characterControllerScript.DirectionalFactor = initialDirectionalFactorValue;
+
+        bool isWithinRange = _characterControllerScript.DirectionalFactor >= -1
+            && _characterControllerScript.DirectionalFactor <= 1;
+
+        Assert.IsTrue(isWithinRange);
+    }
+}
