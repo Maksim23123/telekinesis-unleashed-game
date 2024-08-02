57539e2c57d232f0622f7059754edf0f341f4218
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-08-02 13:22:11 +0200

    Add comments for EmptyItemEvent class

--- Assets/Scripts/Main/Gameplay/Items/Item Events/EmptyItemEvent.cs
+++ Assets/Scripts/Main/Gameplay/Items/Item Events/EmptyItemEvent.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// An example class for item events, not intended for use in actual game logic.
+/// </summary>
 [CreateAssetMenu(menuName = "Item events/Empty Event")] 
 public class EmptyItemEvent : ItemEvent
 {
