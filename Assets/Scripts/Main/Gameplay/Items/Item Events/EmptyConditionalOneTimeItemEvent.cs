344fc5c1d9d82fe0783edefe34105a460802ad97
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-08-02 13:40:19 +0200

    Add comments to EmptyConditionalOneTimeItemEvent class

--- Assets/Scripts/Main/Gameplay/Items/Item Events/EmptyConditionalOneTimeItemEvent.cs
+++ Assets/Scripts/Main/Gameplay/Items/Item Events/EmptyConditionalOneTimeItemEvent.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
+/// <summary>
+/// An example class for conditional item events that supposed to be executed only one time, 
+/// not intended for use in actual game logic.
+/// </summary>
 [CreateAssetMenu(menuName = "Item events/Empty Cinditional One Time Event")] 
 public class EmptyConditionalOneTimeItemEvent : ItemEvent
 {
     [SerializeField] private string output;
     [SerializeField] private float height = 10;
 
+
     public override void ExecuteItemEvent()
     {
         Debug.Log(output);
         EventExecuted = true;
     }
+
 
     public override bool Union(ItemEvent itemEvent)
     {
