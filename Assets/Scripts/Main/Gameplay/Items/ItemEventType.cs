349831cf954e75dea9d0d09a9cd9cf85d8dbbd6a
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-08-05 13:56:17 +0200

    Add comments for ItemEventType enum

--- Assets/Scripts/Main/Gameplay/Items/ItemEventType.cs
+++ Assets/Scripts/Main/Gameplay/Items/ItemEventType.cs
@@ -1,6 +1,18 @@
+/// <summary>
+/// This enum shows how ItemEvent should be executed by ItemIventExecutor.
+/// </summary>
 public enum ItemEventType
 {
+    /// <summary>
+    /// Type of ItemEvent that suposed to be executed only one time.
+    /// </summary>
     OneTime,
+    /// <summary>
+    /// Type of ItemEvent that supposed to be executed continuously under certain conditions.
+    /// </summary>
     Conditional,
+    /// <summary>
+    /// Type of ItemEvent that supposed to be executed only one time under certain conditions.
+    /// </summary>
     ConditionalOneTime
 }
