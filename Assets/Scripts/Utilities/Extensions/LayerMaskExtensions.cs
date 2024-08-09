f43aa744c0ea5e019665fe0083c630417b6fba94
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-08-09 12:14:24 +0200

    Refactor comments for LayerMaskExtensions class

--- Assets/Scripts/Utilities/Extensions/LayerMaskExtensions.cs
+++ Assets/Scripts/Utilities/Extensions/LayerMaskExtensions.cs
@@ -6,7 +6,7 @@ public static class LayerMaskExtensions
     /// Checks if the given layer is included in the LayerMask.
     /// </summary>
     /// <param name="layerMask">The LayerMask to check against.</param>
-    /// <param name="layer">The layer to check for.</param>
+    /// <param name="layer">The layer index to check for (0-31).</param>
     /// <returns>True if the layer is included in the LayerMask, false otherwise.</returns>
     public static bool Contains(this LayerMask layerMask, int layer)
     {
