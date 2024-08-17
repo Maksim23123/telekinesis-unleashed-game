8b618bc878545a6bbf5fcba4a949413801544089
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2024-08-17 15:10:37 +0200

    Add comments to PlayerGameObjectLifeCycleManager class

--- Assets/Scripts/Player/PlayerGameObjectLifeCycleManager.cs
+++ Assets/Scripts/Player/PlayerGameObjectLifeCycleManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// Manages the player's GameObject lifecycle by performing actions on its creation and destruction.
+/// </summary>
 public class PlayerGameObjectLifeCycleManager : MonoBehaviour
 {
     private void Awake()
